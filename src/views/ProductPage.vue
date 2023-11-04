098b0fbce4a96f120f4898c658369c794fa01f25
Екатерина Белкова <belkova.k.2022@gmail.com>
2023-11-04 20:32:33 +0300

    created ProductPage

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    динамическая страница
+  </div>
+</template>
+
+<script>
+// import { ref } from 'vue'
+
+export default {
+  name: 'ProductPage',
+  components: {
+  },
+  props: {
+  },
+  setup () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
